1fc1dc6b79905c13b664ddd6bfc56519095f01fc
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-15 00:18:50 +0500

    updated

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,7 +2,7 @@ import styles from './Calculator.module.css'
 
 const Calculator = () => {
 
-    const buttonNames = ['C', '1', '2', '3', '+', '4', '-', '5', '6', '*', '7', '8', '=', '9', '0', '.',]
+    const buttonNames = ['C', '1', '2', '3', '+', '4', '5', '6', '-', '7', '8','9','*','=','.','0',]
 
     return (
         <>
