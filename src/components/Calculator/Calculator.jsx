f4b213b6e90a38394f1c702cbb6af9debb9d3082
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-15 00:11:35 +0500

    updated

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,33 +2,27 @@ import styles from './Calculator.module.css'
 
 const Calculator = () => {
 
-    const buttonNames = ['','','','','','','','','','','','', ]
-    
-  return (
-    <>
-        <section className={styles.calculator}>
-            <div className={styles.container}>
-                <h1>Calculator</h1>
-                <div className={styles.inputfield}>
-                    <input type="text" name="value" id="value" />
-                    <button className={styles.clearbtn}>C</button>
+    const buttonNames = ['C', '1', '2', '+', '3', '4', '-', '5', '6', '*', '7', '8', '=', '9', '0', '.',]
+
+    return (
+        <>
+            <section className={styles.calculator}>
+                <div className={styles.container}>
+                    <h1>Calculator</h1>
+                    <div className={styles.inputfield}>
+                        <input type="text" name="value" id="value" />
+                        <button className={styles.clearbtn}>C</button>
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        {buttonNames.map((btn) => (
+                            <button key={btn} className={styles.buttonSize}>{btn}</button>
+                        ))}
+
+                    </div>
                 </div>
-                <div className={styles.buttonContainer}>
-                    <button className={styles.buttonSize}>1</button>
-                    <button className={styles.buttonSize}>2</button>
-                    <button className={styles.buttonSize}>3</button>
-                    <button className={styles.buttonSize}>4</button>
-                    <button className={styles.buttonSize}>5</button>
-                    <button className={styles.buttonSize}>6</button>
-                    <button className={styles.buttonSize}>7</button>
-                    <button className={styles.buttonSize}>8</button>
-                    <button className={styles.buttonSize}>9</button>
-                    <button className={styles.buttonSize}>0</button>
-                </div>
-            </div>
-        </section>
-    </>
-  )
+            </section>
+        </>
+    )
 }
 
 export default Calculator
