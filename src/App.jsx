95bdea05eb0b76dd02065da1a31e65c76cda418a
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-14 23:41:27 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-     
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>App</div>
   )
 }
 
