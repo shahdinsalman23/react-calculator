1c91ef6f3e09cec7b7471256ea47e71908038c27
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-15 00:05:31 +0500

    updated

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -10,6 +10,18 @@ const Calculator = () => {
                     <input type="text" name="value" id="value" />
                     <button className={styles.clearbtn}>C</button>
                 </div>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.buttonSize}>1</button>
+                    <button className={styles.buttonSize}>2</button>
+                    <button className={styles.buttonSize}>3</button>
+                    <button className={styles.buttonSize}>4</button>
+                    <button className={styles.buttonSize}>5</button>
+                    <button className={styles.buttonSize}>6</button>
+                    <button className={styles.buttonSize}>7</button>
+                    <button className={styles.buttonSize}>8</button>
+                    <button className={styles.buttonSize}>9</button>
+                    <button className={styles.buttonSize}>0</button>
+                </div>
             </div>
         </section>
     </>
