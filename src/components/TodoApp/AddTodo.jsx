76fd53575274233206503073f1331c88ec37f663
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-16 16:44:01 +0500

    updated

--- src/components/TodoApp/AddTodo.jsx
+++ src/components/TodoApp/AddTodo.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import styles from './Todoapp.module.css'
 
 const AddTodo = ({ onNewitem }) => {
 
-    const [todoName, setTodoName] = useState("")
-    const [dueDate, setDueDate] = useState("")
+    const todoNameElement = useRef();
+    const dueDateElement =  useRef()
 
-    const handleNameChange = (e) =>  {
-        setTodoName(e.target.value)
-    }
 
-    const handleDateChange = (e) =>  {
-        setDueDate(e.target.value)
-    }
-
-    const handleAddButtonClicked = () => {
-        onNewitem(todoName, dueDate)
-        setTodoName("")
-        setDueDate("")
+    const handleAddButtonClicked = (e) => {
+        e.preventDefault();
+        const todoName = todoNameElement.current.value;
+        const dueDate = dueDateElement.current.value;
+        onNewitem(todoName, dueDate);
+        todoNameElement.current.value = '';
+        dueDateElement.current.value = '';
     }
 
 
   return (
-    <div className={styles.addtodo}>
-        <input type="text" value={todoName}  onChange={handleNameChange} />
-        <input type="date" value={dueDate} onChange={handleDateChange} />
-        <button className={styles.addTodoBtn} onClick={handleAddButtonClicked}>Add</button>
-    </div>
+    <form className={styles.addtodo} onSubmit={handleAddButtonClicked}>
+        <input type="text" ref={todoNameElement} />
+        <input type="date" ref={dueDateElement} />
+        <button className={styles.addTodoBtn} type='submit'>Add</button>
+    </form>
   )
 }
 
