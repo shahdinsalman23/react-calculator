9704900b3da0855e008e2a94fa32296e6f99c059
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-16 14:26:00 +0500

    updated

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -6,7 +6,18 @@ import WelccomeMessage from './WelccomeMessage'
 
 const TodoApp = () => {
 
-   
+    const [todoItems, setTodoItems] = useState([]);
+
+    const handleNewItem = (itemName, itemDueDate) => {
+        console.log(`New Item added ${itemName} Date: ${itemDueDate}`)
+        const newTodoItems = [...todoItems, { name: itemName, dueDate: itemDueDate }]
+        setTodoItems(newTodoItems)
+    }
+
+    const handleDeleteItem = (todoItemName) => {
+        let deleteTodoItem = todoItems.filter(item => item.name !== todoItemName.name)
+        setTodoItems(deleteTodoItem);
+    }
 
     return (
         <>
